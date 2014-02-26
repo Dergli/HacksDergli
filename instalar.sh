438888e4b2548df32e55b09c1881b33c4bd81792
Dergli <dergli@hushmail.com>
2014-02-26 10:35:14 +0100

    Translate into English

--- instalar.sh
+++ instalar.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo 1.- Español
+echo 2.- English
+read VAR
+if [ "$VAR" = "1" ]; then
 cp -R ./HacksDergli /home/
 cp /home/HacksDergli/HacksDergli /usr/sbin/hacksdergli
 chmod 755 -R /home/HacksDergli
+elif [ "$VAR" = "2" ]; then
+cp -R ./HacksDergli /home/
+cp /home/HacksDergli/HacksDergliEnglish /usr/sbin/hacksdergli
+chmod 755 -R /home/HacksDergli
+fi
