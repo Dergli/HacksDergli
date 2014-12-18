c3b90364a4c301b952b03eb7febd49fca21403fc
root <root@localhost.localdomain>
2014-12-18 11:30:34 +0100

    Crackear hotspot

--- instalar.sh
+++ instalar.sh
@@ -6,8 +6,10 @@ if [ "$VAR" = "1" ]; then
 cp -R ./HacksDergli /home/
 cp /home/HacksDergli/HacksDergli /usr/sbin/hacksdergli
 chmod 755 -R /home/HacksDergli
+chmod +x /usr/sbin/hacksdergli
 elif [ "$VAR" = "2" ]; then
 cp -R ./HacksDergli /home/
 cp /home/HacksDergli/HacksDergliEnglish /usr/sbin/hacksdergli
 chmod 755 -R /home/HacksDergli
+chmod +x /usr/sbin/hacksdergli
 fi
